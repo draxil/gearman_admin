a242198d7c0b7e969027a99e98ce1ca0466467c3
joe <draxil@gmail.com>
2019-10-16 10:48:05 +0100

    gofmt, documentation tweak

--- gearman_admin.go
+++ gearman_admin.go
@@ -1,33 +1,33 @@
 package gearman_admin
 
-import ( 
-	"net" 
+import (
+	"bufio"
 	"io"
-	"bufio" 
+	"net"
 	"strings"
 )
 
 /*
   Connection to a gearman server
 */
-type Connection struct{
+type Connection struct {
 	net.Conn
 }
 
 /*
   Connect to a gearman server
 
-  ie: 
- 
+  ie:
+
   gma, err := gearman_admin.Connect("tcp", "10.0.50.3:4730")
 
 */
-func Connect( network, address string)( connection *Connection, err error ){
+func Connect(network, address string) (connection *Connection, err error) {
 	connection = &Connection{}
-	connection.Conn, err = net.Dial( network, address )
+	connection.Conn, err = net.Dial(network, address)
 
 	if err != nil {
-		return 
+		return
 	}
 
 	return
@@ -36,24 +36,24 @@ func Connect( network, address string)( connection *Connection, err error ){
 /*
   Query the gearman server as to what workers are available
 */
-func (c *Connection) Workers()( workers []Worker, err error ){
-	c.Write( []byte("workers\n") )
-	lines, err := read_until_stop( c )
+func (c *Connection) Workers() (workers []Worker, err error) {
+	c.Write([]byte("workers\n"))
+	lines, err := read_until_stop(c)
 	if err != nil {
 		return
 	}
-	
-	workers, err = workers_from_lines( lines )
+
+	workers, err = workers_from_lines(lines)
 
 	return
 }
 
-func workers_from_lines( lines []string ) ( workers []Worker, err error){
-	workers = make( []Worker, 0, len(lines) )
+func workers_from_lines(lines []string) (workers []Worker, err error) {
+	workers = make([]Worker, 0, len(lines))
 
-	for _, line := range( lines ) {
-		trimmed_line := strings.TrimRight( line, "\n")
-		parts := strings.Split( trimmed_line, " " )
+	for _, line := range lines {
+		trimmed_line := strings.TrimRight(line, "\n")
+		parts := strings.Split(trimmed_line, " ")
 		if len(parts) < 4 {
 			err = ProtocolError("Incomplete worker entry")
 			return
@@ -65,15 +65,15 @@ func workers_from_lines( lines []string ) ( workers []Worker, err error){
 		}
 
 		var worker Worker
-		
-		worker.Fd        = parts[0]
-		worker.Addr      = parts[1]
-		worker.ClientId  = parts[2]
-		if( len(parts) > 4 ){
+
+		worker.Fd = parts[0]
+		worker.Addr = parts[1]
+		worker.ClientId = parts[2]
+		if len(parts) > 4 {
 			worker.Functions = parts[4:len(parts)]
 		}
-		
-		workers = append( workers, worker )
+
+		workers = append(workers, worker)
 	}
 
 	return
@@ -82,17 +82,17 @@ func workers_from_lines( lines []string ) ( workers []Worker, err error){
 /*
    Decoded description of a gearman worker
 */
-type Worker struct{
-	Fd               string
-	Addr             string 
-	ClientId         string
-	Functions        []string
+type Worker struct {
+	Fd        string   /* File descriptor */
+	Addr      string   /* IP address */
+	ClientId  string   /* Client ID */
+	Functions []string /* List of functions */
 }
 
 /*
-    Check a worker for a particular function
+   Check a worker for a particular function
 */
-func (w *Worker) HasFunction( funcname string ) (bool){
+func (w *Worker) HasFunction(funcname string) bool {
 	for _, v := range w.Functions {
 		if v == funcname {
 			return true
@@ -101,12 +101,11 @@ func (w *Worker) HasFunction( funcname string ) (bool){
 	return false
 }
 
-
-func read_until_stop( r io.Reader )( lines []string, err error ) {
-	rdr := bufio.NewReader( r )
+func read_until_stop(r io.Reader) (lines []string, err error) {
+	rdr := bufio.NewReader(r)
 	stop := false
-	lines = make( []string, 0, 0 )
-	for ! stop {
+	lines = make([]string, 0, 0)
+	for !stop {
 		line := ""
 		line, err = rdr.ReadString('\n')
 
@@ -116,8 +115,8 @@ func read_until_stop( r io.Reader )( lines []string, err error ) {
 		if line == ".\n" {
 			return
 		}
-		
-		lines = append( lines, line )
+
+		lines = append(lines, line)
 	}
 
 	return
@@ -127,6 +126,7 @@ func read_until_stop( r io.Reader )( lines []string, err error ) {
    Some kind of protocol error
 */
 type ProtocolError string
-func ( p ProtocolError) Error()(string){
+
+func (p ProtocolError) Error() string {
 	return "Protocol error: " + string(p)
 }
