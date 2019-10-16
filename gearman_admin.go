6550ba37895d09d2a9ef466f501e174e950f2c8e
joe <draxil@gmail.com>
2019-10-16 15:55:17 +0100

    Very simple status implementation

--- gearman_admin.go
+++ gearman_admin.go
@@ -2,9 +2,11 @@ package gearman_admin
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"net"
 	"strings"
+	"strconv"
 )
 
 /*
@@ -27,6 +29,7 @@ func Connect(network, address string) (connection *Connection, err error) {
 	connection.Conn, err = net.Dial(network, address)
 
 	if err != nil {
+		err = fmt.Errorf("Error connecting to gearman server: %s", err.Error())
 		return
 	}
 
@@ -37,23 +40,111 @@ func Connect(network, address string) (connection *Connection, err error) {
   Query the gearman server as to what workers are available
 */
 func (c *Connection) Workers() (workers []Worker, err error) {
-	c.Write([]byte("workers\n"))
-	lines, err := read_until_stop(c)
+	_, err = c.Write([]byte("workers\n"))
 	if err != nil {
+		err = fmt.Errorf("Error requesting worker list: %s", err.Error())
 		return
 	}
-
+	
+	var lines []string
+	lines, err = read_until_stop(c)
+	if err != nil {
+		err = fmt.Errorf("Error getting worker list: %s", err.Error())
+		return
+	}
+	
 	workers, err = workers_from_lines(lines)
 
 	return
+}
+
+/*
+  Query the gearman server status
+
+  This shows the status of all registered funtions on the server
+
+*/
+func (c *Connection) Status() (statuses []FunctionStatus, err error) {
+	_, err = c.Write([]byte("status\n"))
+	if err != nil {
+		err = fmt.Errorf("Error requesting function status list: %s", err.Error())
+		return
+	}
+
+	var lines []string
+	lines, err = read_until_stop(c)
+
+	if err != nil {
+		err = fmt.Errorf("Error getting function status list: %s", err.Error())
+	}
+	
+	statuses, err = func_statuses_from_lines(lines)
+
+	if err != nil {
+		err = fmt.Errorf("Error getting function status list: %s", err.Error())
+		statuses = []FunctionStatus{}
+	}
+
+	return
+}
+
+func process_line( line string ) []string{
+	trimmed_line := strings.TrimRight(line, "\n")
+	parts := strings.Split(trimmed_line, " ")
+	return parts
+}
+func func_statuses_from_lines(lines []string) (funcs []FunctionStatus, err error){
+	funcs = make([]FunctionStatus, 0, len(lines))
+	for _, line := range lines {
+		parts := process_line(line)
+
+		if len(parts) < 3 {
+			err = ProtocolError("Incomplete status entry only " + strconv.Itoa(len(parts)) + " fields found: " + line)
+			return
+		}
+
+		var fs FunctionStatus
+
+		fs.Name = parts[0]
+
+		var unfinished, running, workers int
+		
+		unfinished, err = strconv.Atoi(parts[1])
+		if err != nil {
+			err = ProtocolError("bad unfinished count format: " + err.Error())
+			return
+		}
+
+		running, err = strconv.Atoi(parts[2])
+
+		if err != nil {
+			err = ProtocolError("bad running count format: " + err.Error())
+			return
+		}
+
+		workers, err = strconv.Atoi(parts[3])
+		if err != nil {
+			err = ProtocolError("bad worker count format: " + err.Error())
+			return
+		}
+ 		
+		fs.UnfinishedJobs = unfinished
+		fs.RunningJobs = running
+		fs.Workers = workers
+		
+		funcs = append(funcs, fs)
+	}
+
+	return
+
 }
 
 func workers_from_lines(lines []string) (workers []Worker, err error) {
 	workers = make([]Worker, 0, len(lines))
 
 	for _, line := range lines {
-		trimmed_line := strings.TrimRight(line, "\n")
-		parts := strings.Split(trimmed_line, " ")
+		parts := process_line(line)
+
 		if len(parts) < 4 {
 			err = ProtocolError("Incomplete worker entry")
 			return
@@ -77,6 +168,14 @@ func workers_from_lines(lines []string) (workers []Worker, err error) {
 	}
 
 	return
+}
+
+
+type FunctionStatus struct {
+	Name string /* Function name */
+	UnfinishedJobs int /* Number of unfinished jobs */
+	RunningJobs int /* Number of running jobs */
+	Workers int /* Number of workers available */
 }
 
 /*
@@ -130,3 +229,11 @@ type ProtocolError string
 func (p ProtocolError) Error() string {
 	return "Protocol error: " + string(p)
 }
+
+/* TODO / IDEAS
+ - Status
+ - shutdown
+ - user access to version
+ - set maxqueue?
+ - Get the version on connect and then offer newer commands where available?
+*/
--- gearman_admin_test.go
+++ gearman_admin_test.go
@@ -1,12 +1,13 @@
 package gearman_admin
 
 import (
-	"testing"
-	"strings"
-	"reflect"
-	"net"
 	"bufio"
 	"log"
+	"net"
+	"reflect"
+	"strconv"
+	"strings"
+	"testing"
 )
 
 const read_until_stop_data = `
@@ -16,30 +17,106 @@ blue.
 .
 `
 
-func TestReadUntilStop( t *testing.T ){
-	rdr := strings.NewReader( read_until_stop_data  )
-	list, err := read_until_stop( rdr )
+func TestReadUntilStop(t *testing.T) {
+	rdr := strings.NewReader(read_until_stop_data)
+	list, err := read_until_stop(rdr)
 
 	if err != nil {
-		t.Error( err )
+		t.Error(err)
 	}
-	
-	if ! reflect.DeepEqual( list ,[]string{ "\n", "blah\n", "blah\n", "blue.\n" }) {
-		t.Error( "Unexpected response from read_until_stop : ", list )
+
+	if !reflect.DeepEqual(list, []string{"\n", "blah\n", "blah\n", "blue.\n"}) {
+		t.Error("Unexpected response from read_until_stop : ", list)
 	}
 
 }
 
+func TestFuncStatusesFromLines(t *testing.T) {
+	good_data := []string{
+		`bb:user_import 0 0 4`,
+		`bb:colour_swap 1 2 4`,
+		`bb:pdf_splitter 0 0 4`,
+		`pews_ping 0 0 1`,
+		`bb:check_user_print_file 1 1 4`,
+		`pews_event 0 0 1`,
+	}
 
-func TestWorkersFromLines( t *testing.T ){
+	funcs, err := func_statuses_from_lines(good_data)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(funcs) != 6 {
+		t.Error("Unexpected number of funcs" + strconv.Itoa(len(funcs)))
+	}
+
+	expect := []FunctionStatus{
+		FunctionStatus{
+			Name : "bb:user_import",
+			UnfinishedJobs : 0,
+			RunningJobs : 0,
+			Workers : 4,
+		},
+		FunctionStatus{
+			Name : "bb:colour_swap",
+			UnfinishedJobs : 1,
+			RunningJobs : 2,
+			Workers : 4,
+		},
+		FunctionStatus{
+			Name : "bb:pdf_splitter",
+			UnfinishedJobs : 0,
+			RunningJobs : 0,
+			Workers : 4,
+		},
+		FunctionStatus{
+			Name : "pews_ping",
+			UnfinishedJobs : 0,
+			RunningJobs : 0,
+			Workers : 1,
+		},
+		FunctionStatus{
+			Name : "bb:check_user_print_file",
+			UnfinishedJobs : 1,
+			RunningJobs : 1,
+			Workers : 4,
+		},
+		FunctionStatus{
+			Name : "pews_event",
+			UnfinishedJobs : 0,
+			RunningJobs : 0,
+			Workers : 1,
+		},
+	}
+
+	for i, v := range(expect) {
+
+		if funcs[i].Name != v.Name {
+			t.Error("Bad func name for ", i);
+		}
+		if funcs[i].UnfinishedJobs != v.UnfinishedJobs {
+			t.Error("Bad unfinished for ", i);
+		}
+		if funcs[i].RunningJobs != v.RunningJobs {
+			t.Error("Bad running  for ", i);
+		}
+		if funcs[i].Workers != v.Workers {
+			t.Error("Bad workers for ", i ," / ", v.Name, " got ", funcs[i].Workers);
+		}
+	}
+
+	
+}
+
+func TestWorkersFromLines(t *testing.T) {
 	good_data := []string{
 		"33 127.0.0.1 qqjxnvrxsnqcrovdqxcjtyztiyzjpk : add sub bb:ot\n",
 		"33 127.0.0.1 - : green blue\n",
 		"33 127.0.0.1 - :\n",
 	}
-	workers, err := workers_from_lines( good_data )
+	workers, err := workers_from_lines(good_data)
 	if err != nil {
-		t.Error( err )
+		t.Error(err)
 		return
 	}
 	if len(workers) != 3 {
@@ -52,21 +129,21 @@ func TestWorkersFromLines( t *testing.T ){
 		t.Error("Wrong address on worker 0")
 	}
 	if workers[0].ClientId != `qqjxnvrxsnqcrovdqxcjtyztiyzjpk` {
-		t.Error("Wrong clientid: " +  workers[0].ClientId)
+		t.Error("Wrong clientid: " + workers[0].ClientId)
 	}
 	if len(workers[0].Functions) != 3 {
 		t.Error("Unexpected number of functions: ", len(workers[0].Functions))
 	}
-	if ! workers[0].HasFunction("add") {
+	if !workers[0].HasFunction("add") {
 		t.Error("Function 'add' not reported by HasFunction")
 	}
-	if ! workers[0].HasFunction("bb:ot") {
+	if !workers[0].HasFunction("bb:ot") {
 		t.Error("Function 'bb:ot' not reported by HasFunction")
 	}
 	if workers[0].HasFunction("green") {
 		t.Error("Function 'green' falsely reported by HasFunction")
 	}
-	if ! workers[1].HasFunction("green") {
+	if !workers[1].HasFunction("green") {
 		t.Error("Function 'green' not reported by HasFunction on the second worker")
 	}
 
@@ -74,38 +151,38 @@ func TestWorkersFromLines( t *testing.T ){
 		"33 127.0.0.1 qqjxnvrxsnqcrovdqxcjtyztiyzjpk : add sub bb:ot",
 		"33 127.0.0.1 -",
 	}
-	
-	_, err = workers_from_lines( bad_data )
+
+	_, err = workers_from_lines(bad_data)
 
 	if err == nil {
 		t.Error("Did not get an error")
 	}
 	_, ispe := err.(ProtocolError)
 
-	if ! ispe {
+	if !ispe {
 		t.Error("Did not get a protocol error")
 	}
 }
 
-func TestOverTCPErr ( t * testing.T){
-	l, err := net.Listen("tcp","127.0.0.1:0")
+func TestOverTCPErr(t *testing.T) {
+	l, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
-		t.Error( err )
+		t.Error(err)
 	}
 	addr := l.Addr().String()
 	w, err := Connect("tcp", addr)
 	if err != nil {
-		t.Error("Failed to connect " + err.Error() )
+		t.Error("Failed to connect " + err.Error())
 	}
 	if w == nil {
 		t.Error("Connecting gave a null connection")
 	}
 	con, err := l.Accept()
 	if err != nil {
-		t.Error( err )
+		t.Error(err)
 	}
-//	wesent := make( chan string )
-	go func(){
+	//	wesent := make( chan string )
+	go func() {
 		_, err := bufio.NewReader(con).ReadString('\n')
 		if err != nil {
 			panic(err)
@@ -114,43 +191,43 @@ func TestOverTCPErr ( t * testing.T){
 		out.WriteString("33 127.0.0.1 - : green blue\n")
 		//out.WriteString(".\n")
 		con.Close()
-	//	wesent <- in
+		//	wesent <- in
 
 	}()
 
-	_, err /*:*/= w.Workers()
+	_, err /*:*/ = w.Workers()
 	if err == nil {
 		t.Error("No EOF error")
 	}
-	if err != nil && err.Error() != "EOF" {
-		t.Error("Non EOF error")
+	if err != nil && err.Error() != "Error getting worker list: EOF" {
+		t.Error("Non EOF error: " + err.Error())
 	}
-/*	s := <- wesent
-	if s != "workersyo\n" {
-		t.Error("We sent: " + s , "  expected \"workers\\n\"") 
-	}*/
-	
+	/*	s := <- wesent
+		if s != "workersyo\n" {
+			t.Error("We sent: " + s , "  expected \"workers\\n\"")
+		}*/
+
 }
 
-func TestOverTCPWorkers ( t * testing.T){
-	l, err := net.Listen("tcp","127.0.0.1:0")
+func TestOverTCPWorkers(t *testing.T) {
+	l, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
-		t.Error( err )
+		t.Error(err)
 	}
 	addr := l.Addr().String()
 	w, err := Connect("tcp", addr)
 	if err != nil {
-		t.Error("Failed to connect " + err.Error() )
+		t.Error("Failed to connect " + err.Error())
 	}
 	if w == nil {
 		t.Error("Connecting gave a null connection")
 	}
 	con, err := l.Accept()
 	if err != nil {
-		t.Error( err )
+		t.Error(err)
 	}
-	wesent := make( chan string )
-	go func(){
+	wesent := make(chan string)
+	go func() {
 		in, err := bufio.NewReader(con).ReadString('\n')
 		log.Println(in)
 		if err != nil {
@@ -170,20 +247,84 @@ func TestOverTCPWorkers ( t * testing.T){
 		t.Error("Unexpected error: " + err.Error())
 	}
 
-	s := <- wesent
+	s := <-wesent
 	if s != "workers\n" {
-		t.Error("We sent: " + s , "  expected \"workers\\n\"") 
+		t.Error("We sent: "+s, "  expected \"workers\\n\"")
 	}
-	
+
 	if len(workers) != 1 {
 		t.Error("Expected one worker")
 	}
-	worker := workers[0];
+	worker := workers[0]
 	if worker.Fd != "33" {
 		t.Error("worker.Fd")
 	}
 	if worker.Addr != "127.0.0.1" {
 		t.Error("worker.Addr")
 	}
-	
+
+}
+
+func TestOverTCPStatus(t *testing.T) {
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Error(err)
+	}
+	addr := l.Addr().String()
+	w, err := Connect("tcp", addr)
+	if err != nil {
+		t.Error("Failed to connect " + err.Error())
+	}
+	if w == nil {
+		t.Error("Connecting gave a null connection")
+	}
+	con, err := l.Accept()
+	if err != nil {
+		t.Error(err)
+	}
+	wesent := make(chan string)
+	go func() {
+		in, err := bufio.NewReader(con).ReadString('\n')
+		log.Println(in)
+		if err != nil {
+			panic(err)
+		}
+		out := bufio.NewWriter(con)
+		out.WriteString("boo 1 2 3\n")
+		out.WriteString(".\n")
+		out.Flush()
+		con.Close()
+		wesent <- in
+
+	}()
+
+	funcs, err := w.Status()
+	if err != nil {
+		t.Error("Unexpected error: " + err.Error())
+	}
+
+	s := <-wesent
+	if s != "status\n" {
+		t.Error("We sent: "+s, "  expected \"status\\n\"")
+	}
+
+	if len(funcs) != 1 {
+		t.Error("Expected one func")
+	}
+	funct := funcs[0]
+	if funct.Name != "boo" {
+		t.Error("funct.Name")
+	}
+	if funct.UnfinishedJobs != 1 {
+		t.Error("funct.UnfinishedJobs")
+	}
+	if funct.RunningJobs != 2 {
+		t.Error("funct.RunningJobs")
+	}
+	if funct.Workers != 3 {
+		t.Error("funct.Workers")
+	}
+
+
+
 }
